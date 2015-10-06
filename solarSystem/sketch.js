67cb2d205959cfd208b9b4c95a83200d35dd206a
jcharry <jamie.charry@gmail.com>
2015-10-05 22:01:11 -0400

    updated solar system sketch

--- solarSystem/sketch.js
+++ solarSystem/sketch.js
@@ -33,8 +33,8 @@ var pluto;
 
 var zoomIn;
 var zoomOut;
-var zoomLevel;
-var pauseButton;
+// var zoomLevel;
+// var pauseButton;
 
 var speedSlider;
 
@@ -44,6 +44,7 @@ var solarSystem;
 var distancePanned;
 
 var isPlaying;
+var dragging;
 
 var aNote;
 var bNote;
@@ -52,6 +53,9 @@ var dNote;
 var eNote
 var fNote;
 var gNote;
+var playPauseImg = [];
+
+var zoomImg = [];
 
 function preload() {
   aNote = loadSound("assets/a.wav");
@@ -61,39 +65,25 @@ function preload() {
   eNote = loadSound("assets/e.wav");
   fNote = loadSound("assets/f.wav");
   gNote = loadSound("assets/g.wav");
-  
+  playPauseImg[0] = loadImage("assets/play.png");
+  playPauseImg[1] = loadImage("assets/pause.png");
+  zoomImg[0] = loadImage("assets/zoomIn.png");
+  zoomImg[1] = loadImage("assets/zoomOut.png");
 }
 
 function setup() {
   createCanvas(1440,900);
   
+  textFont("Futura");
+  
   distancePanned = {x: 0, y: 0};
   
-  // musicLines = [];
-  
   isPlaying = true;
-  
-  zoomIn = createButton("+");
-  zoomOut = createButton("-");
-  zoomIn.position(100, 100);
-  zoomIn.size(30,30);
-  zoomOut.position(100,130);
-  zoomOut.size(30,30);
-  zoomIn.mousePressed(zoomPageIn);
-  zoomOut.mousePressed(zoomPageOut);
   zoomLevel = 1;
   
-  speedSlider = createSlider(0,10,1);
-  speedSlider.position(135,120);
-  
-  pauseButton = createButton("pause");
-  pauseButton.mousePressed(pause);
-  pauseButton.position(width/2, 100);
-  pauseButton.size(60,30);
-  playButton = createButton("play");
-  playButton.mousePressed(play);
-  playButton.position(width/2+70, 100);
-  playButton.size(60,30);
+  speedSlider = createSlider(0.1,10,1);
+  speedSlider.position(1300,190);
+  // speedSlider.color(255,255,255);
   
   createPlanets();
   
@@ -107,9 +97,27 @@ function draw() {
   // Using distancePanned, translate text in opposite direction to ensure it stays still
   push();
   translate(-distancePanned.x, -distancePanned.y);
-  text("Speed", 180, 120);
-  text("Zoom", 100, 90);
+  strokeWeight(1);
+  // stroke(0,0,255);
+  fill(47,70,176);
+  text("speed", 1350, 190);
+  text("zoom", 1350, 90);
+  image(zoomImg[0], 1350, 100, 30, 30);
+  image(zoomImg[1], 1350, 130, 30, 30);
+  
+  textSize(20);
+  text("reset", width-56,20)
+  
+  // set play pause image
+  if (isPlaying) {
+    // display pause button
+    image(playPauseImg[1], width/2-15,120, 50,50);
+  } else {
+    // display play button
+    image(playPauseImg[0], width/2-15,120,50,50);
+  }
   pop();
+  
   
   // Run the Solar System!
   solarSystem.run();
@@ -119,17 +127,39 @@ function draw() {
   } else {
     solarSystem.pause();
   }
-  
 }
 
-function mousePressed() {
-  
-  // ensure mouse is not overlapping any buttons!
-  if (mouseX > 80 && mouseX < 1000 && mouseY > 80 && mouseY < 180) {
-    print("print me");
-  } else {
-    solarSystem.addMusicLine(mouseX - width/2,mouseY - height/2);
+function mouseReleased() {
+  // Check if the mouse is currently being dragged
+  // If not - we can add music lines
+  if (!dragging) {
+    
+    // mouse locations for buttons!
+    if (mouseX > 696 && mouseX < 765 && mouseY > 105 && mouseY < 190) {
+      // playPause button
+      isPlaying = !isPlaying;
+    } else if (mouseX > 1350 && mouseX < 1383 && mouseY > 102 && mouseY < 132) {
+      // Zoom in
+      if (zoomLevel < 9) {
+      zoomLevel++;
+      }
+    } else if (mouseX > 1350 && mouseX < 1383 && mouseY > 133 && mouseY < 163) {
+      // Zoom out
+      if (zoomLevel > 1) {
+        zoomLevel--;
+      }
+    } else if (mouseX > width - 70 && mouseX < width && mouseY > 0 && mouseY < 40) {
+      // reset button!
+      reset();
+    } else if (mouseX > 1273 && mouseX < width && mouseY > 59 && mouseY < 233) {
+      // white space around buttons pressed - Don't draw line!!!
+    } else {
+      // didn't hit a button - add a music line
+      solarSystem.addMusicLine((mouseX - (width/2) - distancePanned.x), (mouseY - (height/2) - distancePanned.y));
+    }
   }
+  // reset dragging flag after mouse released
+  dragging = false;
 
   // This stuff was me trying to figure out how to click on a plnaet and select it
   // It didn't really work out, and I"m abandoning it for now
@@ -166,11 +196,14 @@ function mousePressed() {
     }
   }
   */
-  
 }
 
+// Pan planets around
 function mouseDragged() {
-  if (mouseX > 80 && mouseX < 880 && mouseY > 80 && mouseX < 180) {
+  // Set dragging flag to true to tell mouseReleased() not to add music lines
+  dragging = true;
+  
+  if ((mouseX > 1289) && (mouseX < width) && (mouseY > 176) && (mouseY < 223)) {
   } else {
     var distTraveled = {x: -pwinMouseX + mouseX, y: -pwinMouseY + mouseY};
     distancePanned.x += distTraveled.x;
@@ -184,25 +217,17 @@ function mouseDragged() {
 }
 
 
-// function reset() {
-//   pauseButton("reset tapped");
-//   isPlaying = true;
-// }
+function reset() {
+  solarSystem.resetSystem();
 
-function pause() {
-  isPlaying = false;
 }
-
-function play() {
-  isPlaying = true;
-}
-
 
 function zoomPageIn() {
   if (zoomLevel < 10) {
     zoomLevel++;
   }
 }
+
 function zoomPageOut() {
   if (zoomLevel > 1) {
     zoomLevel--;
@@ -213,18 +238,16 @@ function zoomPageOut() {
 function createPlanets() {
 //  1 (m) : 2.41 (v) : 2.54 (e) : 1.34 (m) : 28.4 (j) : 24 (s) : 10.2 (u) : 9.88 (n) : 1/2.18 (p)
 
-  var sizeScaleFactor = 1;
-  var orbitScaleFactor = 2;
-  // sun = new Planet(10,0,0,0);
-  mercury = new Planet("mercury", 1*sizeScaleFactor,0,0.206,5.79, 2);
-  venus = new Planet("venus", 2.41*sizeScaleFactor,0,0.0068,10.8, 5.11);
-  earth = new Planet("earth", 2.54*sizeScaleFactor,0,0.0167,15, 8.3);
-  mars = new Planet("mars", 1.34*sizeScaleFactor, 0, 0.0934, 22.8, 15.61);
-  jupiter = new Planet("jupiter", 28.4*sizeScaleFactor, 0, 0.0485, 77.8, 98.05);
-  saturn = new Planet("saturn", 24,0*sizeScaleFactor, 0.0556, 143, 244);
-  uranus = new Planet("uranus", 10.2,0*sizeScaleFactor, 0.0472, 287, 696);
-  neptune = new Planet("neptune", 18*sizeScaleFactor,0, 0.0086, 450, 1360);
-  pluto = new Planet("pluto", 0.0022*2.54*sizeScaleFactor, 0, 0.25, 590, 2048);
+  // Create planets with appropriate constants for orbit and size
+  mercury = new Planet("mercury", 1,0,0.206,5.79, 2);
+  venus = new Planet("venus", 2.41,0,0.0068,10.8, 5.11);
+  earth = new Planet("earth", 2.54,0,0.0167,15, 8.3);
+  mars = new Planet("mars", 1.34, 0, 0.0934, 22.8, 15.61);
+  jupiter = new Planet("jupiter", 28.4, 0, 0.0485, 77.8, 98.05);
+  saturn = new Planet("saturn", 24,0, 0.0556, 143, 244);
+  uranus = new Planet("uranus", 10.2,0, 0.0472, 287, 696);
+  neptune = new Planet("neptune", 18,0, 0.0086, 450, 1360);
+  pluto = new Planet("pluto", 0.0022*2.54, 0, 0.25, 590, 2048);
 }
 
 function Planet(name, size, theta, eccentricity, semiMajorAxis, period) {
@@ -238,10 +261,15 @@ function Planet(name, size, theta, eccentricity, semiMajorAxis, period) {
   this.b = this.a*( sqrt( 1-(this.e*this.e) ) ) // semi-minor axis
   this.r = (this.a*(1-(this.e*this.e)))/(1+(this.e*cos(this.theta)));
 
+  // translate polar coords into cartesian
   this.x = this.r*cos(this.theta);
   this.y = this.r*sin(this.theta);
   
-  this.velocity = 1/this.r
+  // text location
+  this.textLocation = {x: this.x + this.scaledSize/2 + 5, y: this.y + 5};
+  
+  // velocity is proportional to radius from center
+  this.velocity = 0.1/this.r
 }
 
 Planet.prototype.update = function() {
@@ -268,6 +296,8 @@ Planet.prototype.update = function() {
     // Translate r into cartesian coords to be used for display
     this.x = this.r*cos(this.theta);
     this.y = this.r*sin(this.theta);
+    this.textLocation.x = this.x + this.scaledSize/2 + 5;
+    this.textLocation.y = this.y + 5;
 }
 
 // Display method to draw planet
@@ -275,24 +305,30 @@ Planet.prototype.display = function() {
   // Draw planet
   fill(240);
   ellipse(this.x, this.y, this.scaledSize, this.scaledSize);
-  text(this.name, this.x + this.scaledSize/2 + 5,this.y);
-  
+
   if (this.name === "jupiter") {
+    this.textLocation.x = this.x + this.scaledSize/2 + 10
     noFill();
     ellipse(this.x, this.y, this.scaledSize + 20, this.scaledSize + 20);
     ellipse(this.x, this.y, this.scaledSize + 25, this.scaledSize + 25);
     ellipse(this.x, this.y, this.scaledSize + 30, this.scaledSize + 30);
   }
   
+  //
+  fill(240);
+  text(this.name, this.textLocation.x,this.textLocation.y);
+
+  
+  // TODO: wanted to add a moon around the earth, eventually
   if (this.name === "earth") {
-    noFill();
     // TODO: Add moon
   }
 }
 
+// Method to check if ball is overlapping a line
 Planet.prototype.checkForMusicLine = function() {
   for (var i = 0; i < solarSystem.musicLines.length; i++) {
-    if (this.theta - solarSystem.musicLines[i].theta < 0.01 && this.theta - solarSystem.musicLines[i].theta > -0.03) {
+    if (this.theta - solarSystem.musicLines[i].theta < 0.005 && this.theta - solarSystem.musicLines[i].theta > -0.005) {
       
       // get length of line, link to different note
       var noteToPlay;
@@ -313,13 +349,13 @@ Planet.prototype.checkForMusicLine = function() {
         noteToPlay = bNote;
       }
       
-      print(this.name);
+      // Switch on name to amplify notes played by certain planets
       switch (this.name) {
         case "mercury": 
-          noteToPlay.setVolume(0.2);
+          noteToPlay.setVolume(0.1);
           break;
         case "venus":
-          noteToPlay.setVolume(0.4);
+          noteToPlay.setVolume(0.2);
           break;
         case "earth": 
           noteToPlay.setVolume(0.3);
@@ -328,23 +364,24 @@ Planet.prototype.checkForMusicLine = function() {
           noteToPlay.setVolume(0.5);
           break;
         case "jupiter":
-          noteToPlay.setVolume(0.6);
+          noteToPlay.setVolume(0.4);
           break;
         case "saturn":
-          noteToPlay.setVolume(0.6);
+          noteToPlay.setVolume(0.3);
           break;
         case "uranus":
-          noteToPlay.setVolume(1);
+          noteToPlay.setVolume(0.5);
           break;
         case "neptune":
-          noteToPlay.setVolume(1);
+          noteToPlay.setVolume(0.8);
           break;
         case "pluto":
-          noteToPlay.setVolume(1);
+          noteToPlay.setVolume(0.6);
           break;
-
       }
       noteToPlay.play();
+      
+      // return to ensure that notes don't get played more than they should
       return;
     }
   }
@@ -400,6 +437,15 @@ SolarSystem.prototype.pause = function() {
   pop();  
 }
 
+SolarSystem.prototype.resetSystem = function() {
+  isPlaying = false;
+  this.musicLines = [];
+  for (var i = 0; i < this.planets.length; i++) {
+    this.planets[i].theta = 0;
+  }
+  isPlaying = true;
+}
+
 function MusicLine(x, y) {
   // TODO: Placement of lines doesn't handle panning properly!!! arg!!!
   this.x = x;
@@ -417,6 +463,7 @@ function MusicLine(x, y) {
 }
 
 MusicLine.prototype.display = function() {
+  stroke(120);
   line(this.x,this.y,0,0);
 }
 
